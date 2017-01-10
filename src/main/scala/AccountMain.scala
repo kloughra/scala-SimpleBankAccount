5a16f2f3509440c7802992cc2d0b6166744d4b42
Katie Loughran <katie.loughran@gmail.com>
2017-01-10 11:30:14 -0500

    Updated main - just to be clear

--- src/main/scala/AccountMain.scala
+++ src/main/scala/AccountMain.scala
@@ -1,46 +1,45 @@
 import bank._
-
+import akka.actor.ActorSystem
 object AccountMain extends App{
 
-  //val system = ActorSystem("System")
-  val me = Person("Zaphod", "Beeblebrox", "77-123-4500")
-  val maybeZaphodsAccount = Account(me) deposit 1000.00
-  val maybeZaphodsAccount2 = maybeZaphodsAccount.get.toAccount withdraw 10
+    val system = ActorSystem("System")
+    val me = Person("Zaphod", "Beeblebrox", "77-123-4500")
+    val maybeZaphodsAccount = Account(me) deposit 1000.00
+    val maybeZaphodsAccount2 = maybeZaphodsAccount.get.toAccount withdraw 10
 
-  println(maybeZaphodsAccount2 map(_.toAccount currentBalance))
+    println(maybeZaphodsAccount2 map(_.toAccount currentBalance))
 
-    /*
-  println(maybeZaphodsAccount2)
-
-
-  // Now a different account.
-  val you = Person("Ford", "Prefect", "00-000-0001")
-  val maybeFordsAccount = Account(you) deposit 5000.00
-  maybeFordsAccount map (_.toAccount currentBalance)
-
-  //Set up
-
-  // Let's do a transfer.
-  val zaphodsAccount = maybeZaphodsAccount2.get.toAccount // Don't do this in the real world. Use getOrElse.
-  val fordsAccount = maybeFordsAccount.get.fromAccount // Nope. Never do it.
-  val maybeFords2Zaphods = fordsAccount transfer(500, zaphodsAccount)
-  maybeFords2Zaphods map (_.toAccount currentBalance)
-  val maybeFordsAccount2 = maybeFordsAccount
-
-  maybeFords2Zaphods map {trans =>
-    println("Fords:")
-    println("Transaction history:")
-    println(s"Current balance: ${trans.fromAccount.currentBalance}")
-    println(trans.fromAccount)
-  }
+    println(maybeZaphodsAccount2)
 
 
-  maybeFords2Zaphods map { trans =>
-    println("Zaphods:")
-    println("Transaction history:")
-    println(s"Current balance: ${trans.toAccount.currentBalance}")
-    println(trans.toAccount)
-  }
-*/
-  Account.end
+    // Now a different account.
+    val you = Person("Ford", "Prefect", "00-000-0001")
+    val maybeFordsAccount = Account(you) deposit 5000.00
+    maybeFordsAccount map (_.toAccount currentBalance)
+
+    //Set up
+
+    // Let's do a transfer.
+    val zaphodsAccount = maybeZaphodsAccount2.get.toAccount // Don't do this in the real world. Use getOrElse.
+    val fordsAccount = maybeFordsAccount.get.fromAccount // Nope. Never do it.
+    val maybeFords2Zaphods = fordsAccount transfer(500, zaphodsAccount)
+    maybeFords2Zaphods map (_.toAccount currentBalance)
+    val maybeFordsAccount2 = maybeFordsAccount
+
+    maybeFords2Zaphods map {trans =>
+        println("Fords:")
+        println("Transaction history:")
+        println(s"Current balance: ${trans.fromAccount.currentBalance}")
+        println(trans.fromAccount)
+    }
+
+
+    maybeFords2Zaphods map { trans =>
+        println("Zaphods:")
+        println("Transaction history:")
+        println(s"Current balance: ${trans.toAccount.currentBalance}")
+        println(trans.toAccount)
+    }
+
+    Account.end()
 }
